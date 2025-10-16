6c589c2e687a2e51569a69b03f74b794ee33b9dc
Perttu Saarsalmi <ext.perttu.saarsalmi@metsagroup.com>
2025-10-16 12:55:01 +0300

    osa11 step9 playwright tests added to piepline

---
+++ tests/example.spec.js
@@ -0,0 +1,11 @@
+const { test, expect } = require('@playwright/test')
+
+test('front page can be opened', async ({ page }) => {
+  await page.goto('')
+  await expect(page.getByText('ivysaur')).toBeVisible()
+  await expect(
+    page.getByText(
+      'Pokémon and Pokémon character names are trademarks of Nintendo.'
+    )
+  ).toBeVisible()
+})
